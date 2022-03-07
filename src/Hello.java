fc37949b046445c889078262fcbad12708bfa6f0
JinEuntack <rt4606@naver.com>
2022-03-07 21:10:52 +0900

    shin

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,8 @@
-//소스파일의 이름과 클래스의 이름은 같아야한다!
+//한글 확인
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("ㅋㅋㅋㅋㅋㅋㅋ");
-		System.out.println("깃허브 개쩐다 ");
+		System.out.println("ㄴ");
+		System.out.println("ㅁㅁㅁㄴ ");
 	}
 }
-// full test
+// full test 2222
